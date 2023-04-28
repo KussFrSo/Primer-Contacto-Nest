abccae2b76b75ebf5065a90164b1142a48875bb3
KussFrSo <marcfranch.fh@gmail.com>
2023-04-28 12:36:26 +0200

    [Task] Changed hardcoded datasource vars for env vars

--- src/database/dataSource.ts
+++ src/database/dataSource.ts
@@ -1,12 +1,15 @@
+import * as dotenv from 'dotenv';
 import { DataSource } from 'typeorm';
+
+dotenv.config({ path: 'env/.env.dev' });
 
 export const connectionSource = new DataSource({
   type: 'mysql',
-  host: 'localhost',
-  port: 3306,
-  username: 'root',
-  password: '',
-  database: 'test',
+  host: process.env.SQL_DB_HOST,
+  port: parseInt(process.env.SQL_DB_PORT),
+  username: process.env.SQL_DB_USERNAME,
+  password: process.env.SQL_DB_PASSWORD,
+  database: process.env.SQL_DB_NAME,
   logging: true,
   synchronize: false,
   entities: ['src/**/*.schema.ts'],
